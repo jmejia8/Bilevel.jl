9497dcbabd205fe6439e5a925893687f2362c86d
Jesus Mejia <jesusmejded@gmail.com>
2018-12-05 09:33:57 -0600

    making stats

--- test/offline_test.jl
+++ test/offline_test.jl
@@ -8,6 +8,10 @@ include("../src/operators.jl")
 
 include("../src/qca.jl")
 
+using CSVanalyzer
+import DelimitedFiles.writedlm
+
+
 function getBilevel(fnum)
     D_ul = D_ll = 5
 
@@ -27,7 +31,7 @@ function test()
 
 
 
-    for fnum = 1:8
+    for fnum = 6:7
         F, f, bounds_ul, bounds_ll = getBilevel(fnum)
 
         P, b, iters, nevals_ul, nevals_ll = optimize(F, f, bounds_ul = bounds_ul, bounds_ll=bounds_ll)
@@ -39,5 +43,70 @@ function test()
 
 end
 
+function configure(output_dir="./")
+    all_data_dir = string(output_dir, "output")
+    summary_data_dir = string(output_dir, "summary")
+    
+    !isdir(all_data_dir)     && mkdir(all_data_dir)
+    !isdir(summary_data_dir) && mkdir(summary_data_dir)
 
-test()
+    # println("Using ", Threads.nthreads(), " threads.")
+end
+
+function saveData(output_dir="./")
+
+    configure()
+
+    NFUNS = 8
+    NRUNS = 31
+
+    errors_UL = zeros(NFUNS, NRUNS)
+    errors_LL = zeros(NFUNS, NRUNS)
+
+    evals_UL = zeros(NFUNS, NRUNS)
+    evals_LL = zeros(NFUNS, NRUNS)
+
+
+    for fnum = 1:8
+        
+        F, f, bounds_ul, bounds_ll = getBilevel(fnum)
+        for i = 1:NRUNS
+
+            P, best, iters, nevals_ul, nevals_ll = optimize(F, f, bounds_ul = bounds_ul, bounds_ll=bounds_ll)
+        
+            @printf("SMD%d \t F = %e \t f = %e \t ev_ul = %d \t ev_ll = %d\n", fnum, best.F, best.f, nevals_ul, nevals_ll)
+
+            errors_UL[fnum, i] = best.F; errors_LL[fnum, i] = best.f
+            evals_UL[fnum, i] = nevals_ul
+            evals_LL[fnum, i] = nevals_ll
+        end
+
+        println("-----------------------------------------------")
+
+    end
+
+    writedlm("output/accuracy_UL.csv", errors_UL, ',')
+    writedlm("output/accuracy_LL.csv", errors_LL, ',')
+
+    writedlm("output/evals_UL.csv", evals_UL, ',')
+    writedlm("output/evals_LL.csv", evals_LL, ',')
+
+    ##
+    println("Upper level")
+    statsToLatex("output/accuracy_UL.csv"; mapping= x->abs.(x))
+    println("-------------------------------------------------")
+
+    println("Lower level")
+    statsToLatex("output/accuracy_LL.csv"; mapping= x->abs.(x))
+    println("-------------------------------------------------")
+
+
+    println("Evaluations")
+    statsToLatex("output/evals_UL.csv"; mapping= x->abs.(x))
+    println("-------------------------------------------------")
+
+
+end
+
+
+saveData()
