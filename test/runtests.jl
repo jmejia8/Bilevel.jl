ed68377aaf3d050b890939fb009d42d2bd5f7913
Jesús Mejía <jesusmejded@gmail.com>
2019-02-09 16:29:26 -0600

    test passed

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,14 +7,13 @@ function test1()
 
     bounds = Matrix([-1.0  1.0]')
 
-    method = QBCA(F, f, bounds,bounds)
+    method = QBCA(F, f, bounds,bounds, s_min=-1, N=20, F_calls_limit = 10000)
 
     result = optimize(method)
 
     best = result.best_sol
-    println(best.F, " ", best.f)
 
-    (best.F - 0.82) < 01e-2 && best.f ≈ 0.0 
+    ≈(best.F, 0.8, atol= 1e-1) && ≈(best.f, 0.0, atol=1e-5) 
 end
 
 @test test1()
