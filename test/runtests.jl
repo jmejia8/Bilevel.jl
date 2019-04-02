50b6be7788b3de60631f17915bb4c85e6d70b96e
Jesus Mejia <jesusmejded@gmail.com>
2019-04-02 12:47:50 -0600

    testing new options

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using Bilevel
 using Test
 
+F(x, y) = sum(x.^2 + 0.1cos.(4π*x) + y.^2 + 0.1sin.(4π*y))
+f(x, y) = sum((x.^2 + y.^2 .- 1.0).^2)
+
 function test1()
-    F(x, y) = sum(x.^2 + 0.1cos.(4π*x) + y.^2 + 0.1sin.(4π*y))
-    f(x, y) = sum((x.^2 + y.^2 .- 1.0).^2)
 
     bounds = Matrix([-1.0  1.0]')
 
@@ -16,4 +17,16 @@ function test1()
     ≈(best.F, 0.8, atol= 1e-1) && ≈(best.f, 0.0, atol=1e-5) 
 end
 
+function test2()
+    bounds = Matrix([-1.0  1.0]')
+
+    method = QBCA(size(bounds, 2); s_min=-1, N=20, F_calls_limit = 10000, ll_iterations = 10, options = Bilevel.Options(store_convergence = true))
+
+    result = optimize(F, f, bounds,bounds, method)
+
+    best = result.best_sol
+    ≈(best.F, 0.8, atol= 1e-1) && ≈(best.f, 0.0, atol=1e-5) && length(result.convergence) > 0
+end
+
 @test test1()
+@test test2()
